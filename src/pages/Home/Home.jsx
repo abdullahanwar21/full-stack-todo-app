04e4555980144285893cfd50f431456eaa1ce391
abdullahanwar21 <abdullahmeer396@gmail.com>
2024-01-22 02:51:21 +0500

    fixed the empty input

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,13 +10,15 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db } from "../../config/firebase/config";
-import { TextField, Box, Button, Typography } from "@mui/material";
+import { TextField, Box, Button, Typography, Alert } from "@mui/material";
 import ImgMediaCard from "../../components/Card";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../config/firebase/config";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const [currentUser, setCurrentUser] = useState(null);
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -31,7 +33,11 @@ const Home = () => {
 
   const handleForm = async (e) => {
     e.preventDefault();
-
+    if(addTodo.current.value === "" ){
+      setErrorMessage("Please enter Some value") 
+      return ;
+    }
+    setErrorMessage() 
     try {
       const docRef = await addDoc(collection(db, "todos"), {
         title: addTodo.current.value,
@@ -90,6 +96,8 @@ const Home = () => {
     <>
       <Box className="container d-flex justify-content-center align-items-center gap-5 flex-column mt-5">
         <Typography variant="h3">Add Todo</Typography>
+        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+
         <Box className="row">
           <form onSubmit={handleForm}>
             <TextField
